f1ece72efa28208a7aee2ca0027ea9dccbe7f351
reloaded <59805910+nik-kl@users.noreply.github.com>
2023-04-26 21:39:13 +0200

    Add files via upload

---
+++ serial_to_csv.py
@@ -0,0 +1,37 @@
+#! /usr/bin/python3
+
+import serial
+import csv
+
+ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+filename = "mac_data.csv"
+header = ["MAC", "RSSI"]
+
+with open(filename, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+
+    scanning = False
+    flag = False
+
+    while True:
+        line = ser.readline().decode().strip()
+
+        if line == "=====":
+            scanning = True
+	    flag = True
+            continue
+
+        if line == "-----":
+	    if flag = True
+                scanning = False
+                break
+
+        if scanning:
+            try:
+                mac, rssi = line.split('/')
+                writer.writerow([mac, rssi])
+            except ValueError:
+                print(f"Invalid line: {line}")
+                continue
