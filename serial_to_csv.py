36e3c5d6ec786c11e49b0feca05c5359dd1940f8
Benjamin <benjamin.engler@proton.me>
2023-11-17 15:01:27 +0100

    continouse data collection

--- serial_to_csv.py
+++ serial_to_csv.py
@@ -4,8 +4,8 @@ import csv
 import time
 import serial
 
-ser = serial.Serial('/dev/ttyUSB0', 115200)
-#ser = serial.Serial('COM5', 115200)
+#ser = serial.Serial('/dev/ttyUSB0', 115200)
+ser = serial.Serial('COM5', 115200)
 
 filename = "mac_data.csv"
 header = ["TIME", "MAC", "RSSI"]
@@ -19,7 +19,11 @@ with open(filename, 'w', newline='') as file:
     scanning = False
     flag = False
 
-    while True:
+file.close()
+
+while True:
+       with open(filename, 'a', newline='') as file:
+        writer = csv.writer(file)
         line = ser.readline().decode().strip()
 
         if line == "W=====":
@@ -31,7 +35,6 @@ with open(filename, 'w', newline='') as file:
         if line == "-----":
             if flag == True:
                 scanning = False
-                break
         if scanning:
             try:
                 mac, rssi = line.split('/')
